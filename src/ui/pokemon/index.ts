e5bf8c849e3617780ac4812ae9348d0e53a7fada
jayce.thai <phat.tnh@carehealth.io>
2025-03-22 20:20:16 +0700

    [TASK-5][API] Integrate API Query Pokemon Listing & Pokemon Detail #5

--- src/ui/pokemon/index.ts
+++ src/ui/pokemon/index.ts
@@ -1,5 +1,7 @@
 export { default as PokemonCard } from "./pokemon-card.ui";
 export { default as PokemonListing } from "./pokemon-listing.ui";
 export { default as PokemonGreeting } from "./pokemon-greeting.ui";
-
+export { default as PokemonListingSSR } from "./pokemon-listing-ssr.ui";
 export { default as PokemonFilterBox } from "./pokemon-filter-box.ui";
+
+export { default as PokemonPaginationBoxSSR } from "./pokemon-pagination-box-ssr.ui";
--- src/ui/pokemon/pokemon-pagination-box.ui.tsx
+++ src/ui/pokemon/pokemon-pagination-box.ui.tsx
@@ -1,40 +1,31 @@
 "use client";
 
+import Link from "next/link";
+
 export type IPokemonPaginationBox = {
-  next: string | null;
-
-  previous: string | null;
-
-  onClickNext?: () => void;
-
-  onClickPrevious?: () => void;
+  page: number;
+  totalPages: number;
 };
 export default function PokemonPaginationBox({
-  next,
-  previous,
-  onClickNext,
-  onClickPrevious,
+  page,
+  totalPages,
 }: IPokemonPaginationBox) {
+  const previous = page > 1 ? page - 1 : page;
+  const next = page < totalPages ? page + 1 : page;
   return (
     <div className="flex flex-row justify-center items-center gap-5 m-5">
-      {previous && (
-        <button
-          className="bg-blue-500 text-white px-2 py-2 min-w-20 rounded-4"
-          onClick={() =>
-            typeof onClickPrevious === "function" && onClickPrevious()
-          }
-        >
-          Previous
-        </button>
-      )}
-      {next && (
-        <button
-          className="bg-blue-500 text-white px-2 py-2 min-w-20 rounded-4"
-          onClick={() => typeof onClickNext === "function" && onClickNext()}
-        >
-          Next
-        </button>
-      )}
+      <Link
+        href={`/pokemon-csr?page=${previous}`}
+        className={`bg-blue-500 text-white px-2 py-2 min-w-20 rounded-4 text-center ${page === 1 && "cursor-not-allowed opacity-50"}`}
+      >
+        Previous
+      </Link>
+      <Link
+        href={`/pokemon-csr?page=${next}`}
+        className={`bg-blue-500 text-white px-2 py-2 min-w-20 rounded-4 text-center ${page === totalPages && "cursor-not-allowed opacity-50"}`}
+      >
+        Next
+      </Link>
     </div>
   );
 }
