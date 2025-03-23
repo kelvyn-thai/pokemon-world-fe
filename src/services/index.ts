826ec2fe67a9b88dc15d2cd6d1d4dd8fae4e12e5
jayce.thai <phat.tnh@carehealth.io>
2025-03-23 10:31:57 +0700

    [TASK-8] Clean up code #8

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,3 @@
-export * from "./auth.service";
-export { default as AuthServices } from "./auth.service";
-
 export * from "./github.service";
 export { default as GithubService } from "./github.service";
 
